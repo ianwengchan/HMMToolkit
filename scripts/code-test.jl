b3e8792819ac200986884d0481e31ab73575bd9b
Sophia Chan <ianweng.chan@gmail.com>
2023-08-17 17:18:42 -0400

    prelim normal expert

--- scripts/code-test.jl
+++ scripts/code-test.jl
@@ -17,11 +17,12 @@ include(srcdir("CTHMM-decode-forward-backward.jl"))
 include(srcdir("CTHMM-decode-viterbi.jl"))
 include(srcdir("CTHMM-batch-decode.jl"))
 include(srcdir("CTHMM-learn-nij-taui.jl"))
+include(srcdir("CTHMM-learn-Q.jl"))
 
 df_longer = load(datadir("df_longer.jld2"), "df_longer")
 
 # test with a CTHMM with 4 states, initiate Q
-Q_mat = [-1.5 0.5 0.5 0.5;
+Q_mat0 = [-1.5 0.5 0.5 0.5;
         0.1 -0.3 0.1 0.1;
         0.2 0.2 -0.6 0.2;
         0.5 0.5 0.5 -1.5]
@@ -29,46 +30,87 @@ Q_mat = [-1.5 0.5 0.5 0.5;
 state_init_prob_list = [0.5; 0.25; 0.15; 0.1]
 
 
-time_interval_list = df_longer.time_interval
+# time_interval_list = df_longer.time_interval
 
-distinct_time_list = CTHMM_precompute_distinct_time_list(time_interval_list)
-# OK
+# distinct_time_list = CTHMM_precompute_distinct_time_list(time_interval_list)
+# # OK
+
+# distinct_time_Pt_list = CTHMM_precompute_distinct_time_Pt_list(distinct_time_list, Q_mat)
+# # OK
 
 
-distinct_time_Pt_list = CTHMM_precompute_distinct_time_Pt_list(distinct_time_list, Q_mat)
-# OK
+# group_df = groupby(df_longer, :ID)
 
-
-group_df = groupby(df_longer, :ID)
-
-num_time_series = size(group_df, 1)
-num_state = 4
+# num_time_series = size(group_df, 1)
+# num_state = 4
     
-obs_seq_emiss_list = CTHMM_precompute_batch_data_emission_prob(df_longer)
-# OK
+# obs_seq_emiss_list = CTHMM_precompute_batch_data_emission_prob(df_longer)
+# # OK
 
 
-g = 1
-seq_df = group_df[g]
-data_emiss_prob_list = obs_seq_emiss_list[g][1]
-log_data_emiss_prob_list = obs_seq_emiss_list[g][2]
+# g = 1
+# seq_df = group_df[g]
+# data_emiss_prob_list = obs_seq_emiss_list[g][1]
+# log_data_emiss_prob_list = obs_seq_emiss_list[g][2]
 
-# log_prob from soft decoding is better than that from hard decoding
-Evij, log_prob = CTHMM_decode_forward_backward(seq_df, data_emiss_prob_list, Q_mat, state_init_prob_list)
-# OK
-best_state_seq, best_log_prob = CTHMM_decode_viterbi(seq_df, log_data_emiss_prob_list, Q_mat, state_init_prob_list)
-# OK
+# num_state = size(Q_mat, 1)
+# len_time_series = nrow(seq_df)
+# time_interval_list = collect(skipmissing(seq_df.time_interval))
+
+# distinct_time_list = CTHMM_precompute_distinct_time_list(time_interval_list)
+# distinct_time_Pt_list = CTHMM_precompute_distinct_time_Pt_list(distinct_time_list, Q_mat)
+
+# # log_prob from soft decoding is better than that from hard decoding
+# Svi, Evij, log_prob = CTHMM_decode_forward_backward(seq_df, data_emiss_prob_list, Q_mat, state_init_prob_list)
+# # OK
+# best_state_seq, best_log_prob = CTHMM_decode_viterbi(seq_df, log_data_emiss_prob_list, Q_mat, state_init_prob_list)
+# # OK
+# cur_all_subject_prob, Etij = CTHMM_batch_decode_Etij_for_subjects(1, df_longer, Q_mat, state_init_prob_list)
+# # OK
 
 
-cur_all_subject_prob, Etij = CTHMM_batch_decode_Etij_for_subjects(1, df_longer, Q_mat, state_init_prob_list)
-# OK
+# Nij_mat, taui_list = CTHMM_learn_nij_taui(distinct_time_list, distinct_time_Pt_list, Q_mat, Etij)
+# # OK
 
+# Q_mat_new = CTHMM_learn_update_Q_mat(Nij_mat, taui_list)
+
+
+df = copy(df_longer)
+Q_mat_init = copy(Q_mat0)
+
+distinct_time_list = CTHMM_precompute_distinct_time_list(df.time_interval)
+num_state = size(Q_mat_init, 1)
+num_distinct_time = size(distinct_time_list, 1) # assume one Q for all time series
+# have to redo for updated Q and distribution parameters:
+distinct_time_Pt_list = CTHMM_precompute_distinct_time_Pt_list(distinct_time_list, Q_mat_init)  # with initial guess Q0
+obs_seq_emiss_list = CTHMM_precompute_batch_data_emission_prob(df)  # with initial parameter guess
+
+pre_all_subject_prob = -Inf
+model_iter_count = 0
+
+Q_mat = copy(Q_mat_init)
+
+model_iter_count = model_iter_count + 1
+
+cur_all_subject_prob, Etij = CTHMM_batch_decode_Etij_for_subjects(1, df, Q_mat, state_init_prob_list)
 
 Nij_mat, taui_list = CTHMM_learn_nij_taui(distinct_time_list, distinct_time_Pt_list, Q_mat, Etij)
-# OK
-
-Nij_mat
-taui_list
-
 Q_mat_new = CTHMM_learn_update_Q_mat(Nij_mat, taui_list)
 
+ye = df.delta_radian
+Y_e_obs = ye
+Y_sq_e_obs = ye .^ 2
+
+z_e_obs = df.Sv1
+term_zkz = z_e_obs
+term_zkz_Y = (z_e_obs .* Y_e_obs)
+term_zkz_Y_sq = (z_e_obs .* Y_sq_e_obs)
+
+μ_new = sum(skipmissing(term_zkz_Y))[1] / sum(skipmissing(term_zkz))[1]
+demominator = sum(skipmissing(term_zkz))[1]
+
+numerator = sum(skipmissing(term_zkz_Y_sq))[1] - 2.0 * μ_new * sum(skipmissing(term_zkz_Y))[1] +
+                (μ_new)^2 * sum(skipmissing(term_zkz))[1]
+
+tmp = numerator / demominator
+σ_new = sqrt(maximum([0.0, tmp]))
---
+++ src/experts/normal.jl
@@ -0,0 +1,265 @@
+"""
+    NormalExpert(μ, σ)
+
+PDF:
+
+```math
+f(x; \\mu, \\sigma) = \\frac{1}{\\sqrt{2 \\pi \\sigma^2}}
+\\exp \\left( - \\frac{(\\x - \\mu)^2}{2 \\sigma^2} \\right)
+```
+
+See also: [Normal Distribution](https://en.wikipedia.org/wiki/Normal_distribution) (Wikipedia)
+
+"""
+struct NormalExpert{T<:Real} <: NonZIContinuousExpert
+    μ::T
+    σ::T
+    NormalExpert{T}(µ::T, σ::T) where {T<:Real} = new{T}(µ, σ)
+end
+
+function NormalExpert(μ::T, σ::T; check_args=true) where {T<:Real}
+    check_args && @check_args(NormalExpert, σ >= zero(σ))
+    return NormalExpert{T}(μ, σ)
+end
+
+## Outer constructors
+NormalExpert(μ::Real, σ::Real) = NormalExpert(promote(μ, σ)...)
+NormalExpert(μ::Integer, σ::Integer) = NormalExpert(float(μ), float(σ))
+NormalExpert() = NormalExpert(0.0, 1.0)
+
+## Conversion
+function convert(::Type{NormalExpert{T}}, μ::S, σ::S) where {T<:Real, S<:Real}
+    return NormalExpert(T(μ), T(σ))
+end
+function convert(::Type{NormalExpert{T}}, d::NormalExpert{S}) where {T<:Real, S<:Real}
+    return NormalExpert(T(d.μ), T(d.σ); check_args=false)
+end
+copy(d::NormalExpert) = NormalExpert(d.μ, d.σ; check_args=false)
+
+## Loglikelihood of Expert
+function logpdf(d::NormalExpert, x...)
+    return Distributions.logpdf.(Distributions.Normal(d.μ, d.σ), x...)
+end
+pdf(d::NormalExpert, x...) = Distributions.pdf.(Distributions.Normal(d.μ, d.σ), x...)
+function logcdf(d::NormalExpert, x...)
+    return Distributions.logcdf.(Distributions.Normal(d.μ, d.σ), x...)
+end
+cdf(d::NormalExpert, x...) = Distributions.cdf.(Distributions.Normal(d.μ, d.σ), x...)
+
+## expert_ll, etc
+expert_ll_exact(d::NormalExpert, x::Real) = LRMoE.logpdf(d, x)
+function expert_ll(d::NormalExpert, tl::Real, yl::Real, yu::Real, tu::Real)
+    expert_ll = if (yl == yu)
+        logpdf.(d, yl)
+    else
+        logcdf.(d, yu) + log1mexp.(logcdf.(d, yl) - logcdf.(d, yu))
+    end
+    expert_ll = (tu == 0.0) ? -Inf : expert_ll
+    return expert_ll
+end
+# function expert_tn(d::NormalExpert, tl::Real, yl::Real, yu::Real, tu::Real)
+#     expert_tn = if (tl == tu)
+#         logpdf.(d, tl)
+#     else
+#         logcdf.(d, tu) + log1mexp.(logcdf.(d, tl) - logcdf.(d, tu))
+#     end
+#     expert_tn = (tu == 0.0) ? -Inf : expert_tn
+#     return expert_tn
+# end
+# function expert_tn_bar(d::NormalExpert, tl::Real, yl::Real, yu::Real, tu::Real)
+#     expert_tn_bar = if (tl == tu)
+#         0.0
+#     else
+#         log1mexp.(logcdf.(d, tu) + log1mexp.(logcdf.(d, tl) - logcdf.(d, tu)))
+#     end
+#     return expert_tn_bar
+# end
+
+exposurize_expert(d::NormalExpert; exposure=1) = d
+
+## Parameters
+params(d::NormalExpert) = (d.μ, d.σ)
+function params_init(y, d::NormalExpert)
+    # pos_idx = (y .> 0.0)  # Normal distribution takes negative values as well
+    μ_init, σ_init = mean(y), sqrt(var(y))
+    μ_init = isnan(μ_init) ? 0.0 : μ_init
+    σ_init = isnan(σ_init) ? 1.0 : σ_init
+    return NormalExpert(μ_init, σ_init)
+end
+
+# ## KS stats for parameter initialization
+# function ks_distance(y, d::NormalExpert)
+#     p_zero = sum(y .== 0.0) / sum(y .>= 0.0)
+#     return max(
+#         abs(p_zero - 0.0),
+#         (1 - 0.0) *
+#         HypothesisTests.ksstats(y[y .> 0.0], Distributions.Normal(d.μ, d.σ))[2],
+#     )
+# end
+
+# ## Simululation
+# sim_expert(d::NormalExpert) = Distributions.rand(Distributions.Normal(d.μ, d.σ), 1)[1]
+
+# ## penalty
+# penalty_init(d::NormalExpert) = [2.0 2.0]
+# no_penalty_init(d::NormalExpert) = [1.0 1.0]
+# penalize(d::NormalExpert, p) = -0.5 * (p[1] - 1) / (d.σ * d.σ) - (p[2] - 1) * log(d.σ)
+
+## statistics
+mean(d::NormalExpert) = mean(Distributions.Normal(d.μ, d.σ))
+var(d::NormalExpert) = var(Distributions.Normal(d.μ, d.σ))
+quantile(d::NormalExpert, p) = quantile(Distributions.Normal(d.μ, d.σ), p)
+# function lev(d::NormalExpert, u)
+#     if isinf(u)
+#         return mean(d)
+#     else
+#         return exp(d.μ + 0.5 * d.σ^2) * cdf.(Normal(d.μ + d.σ^2, d.σ), log(u)) +
+#                u * (1 - cdf.(Normal(d.μ, d.σ), log(u)))
+#     end
+# end
+# excess(d::NormalExpert, u) = mean(d) - lev(d, u)
+
+# ## Misc functions for E-Step
+# function _diff_dens_series(d::NormalExpert, yl, yu)
+#     return exp(-0.5 * (log(yl) - d.μ)^2 / (d.σ^2)) - exp(-0.5 * (log(yu) - d.μ)^2 / (d.σ^2))
+# end
+
+# function _diff_dist_series(d::NormalExpert, yl, yu)
+#     return (0.5 + 0.5 * erf((log(yu) - d.μ) / (sqrt2 * d.σ))) -
+#            (0.5 + 0.5 * erf((log(yl) - d.μ) / (sqrt2 * d.σ)))
+# end
+
+# function _int_obs_logY(d::NormalExpert, yl, yu, expert_ll)
+#     if yl == yu
+#         return log(yl)
+#     else
+#         return exp(-expert_ll) * (
+#             d.σ * invsqrt2π * _diff_dens_series(d, yl, yu) +
+#             d.μ * _diff_dist_series(d, yl, yu)
+#         )
+#     end
+# end
+
+# function _int_lat_logY(d::NormalExpert, tl, tu, expert_tn_bar)
+#     return exp(-expert_tn_bar) * (
+#         d.μ - (
+#             d.σ * invsqrt2π * _diff_dens_series(d, tl, tu) +
+#             d.μ * _diff_dist_series(d, tl, tu)
+#         )
+#     )
+# end
+
+# function _zdensz_series(z)
+#     return (isinf(z) ? 0.0 : z * exp(-0.5 * z^2))
+# end
+
+# function _diff_zdensz_series(d::NormalExpert, yl, yu)
+#     return _zdensz_series((log(yl) - d.μ) / d.σ) - _zdensz_series((log(yu) - d.μ) / d.σ)
+# end
+
+# function _int_obs_logY_sq(d::NormalExpert, yl, yu, expert_ll)
+#     if yl == yu
+#         return (log(yl))^2
+#     else
+#         return exp(-expert_ll) * (
+#             ((d.σ)^2 + (d.μ)^2) * _diff_dist_series(d, yl, yu) +
+#             2.0 * d.σ * d.μ * invsqrt2π * _diff_dens_series(d, yl, yu) +
+#             (d.σ)^2 * invsqrt2π * _diff_zdensz_series(d, yl, yu)
+#         )
+#     end
+# end
+
+# function _int_lat_logY_sq(d::NormalExpert, tl, tu, expert_tn_bar)
+#     return exp(-expert_tn_bar) * (
+#         ((d.σ)^2 + (d.μ)^2) - (
+#             ((d.σ)^2 + (d.μ)^2) * _diff_dist_series(d, tl, tu) +
+#             2.0 * d.σ * d.μ * invsqrt2π * _diff_dens_series(d, tl, tu) +
+#             (d.σ)^2 * invsqrt2π * _diff_zdensz_series(d, tl, tu)
+#         )
+#     )
+# end
+
+## EM: M-Step
+# function EM_M_expert(d::NormalExpert,
+#     tl, yl, yu, tu,
+#     exposure,
+#     z_e_obs, z_e_lat, k_e;
+#     penalty=true, pen_pararms_jk=[1.0 1.0])
+
+#     expert_ll_pos = expert_ll.(d, tl, yl, yu, tu)
+#     expert_tn_bar_pos = expert_tn_bar.(d, tl, yl, yu, tu)
+
+#     # Further E-Step
+#     logY_e_obs = vec(_int_obs_logY.(d, yl, yu, expert_ll_pos))
+#     logY_e_lat = vec(_int_lat_logY.(d, tl, tu, expert_tn_bar_pos))
+#     nan2num(logY_e_lat, 0.0) # get rid of NaN
+
+#     logY_sq_e_obs = vec(_int_obs_logY_sq.(d, yl, yu, expert_ll_pos))
+#     logY_sq_e_lat = vec(_int_lat_logY_sq.(d, tl, tu, expert_tn_bar_pos))
+#     nan2num(logY_sq_e_lat, 0.0) # get rid of NaN
+
+#     # Update parameters
+#     pos_idx = (yu .!= 0.0)
+#     term_zkz = z_e_obs[pos_idx] .+ (z_e_lat[pos_idx] .* k_e[pos_idx])
+#     term_zkz_logY =
+#         (z_e_obs[pos_idx] .* logY_e_obs[pos_idx]) .+
+#         (z_e_lat[pos_idx] .* k_e[pos_idx] .* logY_e_lat[pos_idx])
+#     term_zkz_logY_sq =
+#         (z_e_obs[pos_idx] .* logY_sq_e_obs[pos_idx]) .+
+#         (z_e_lat[pos_idx] .* k_e[pos_idx] .* logY_sq_e_lat[pos_idx])
+
+#     μ_new = sum(term_zkz_logY)[1] / sum(term_zkz)[1]
+
+#     demominator = penalty ? (sum(term_zkz)[1] + (pen_pararms_jk[2] - 1)) : sum(term_zkz)[1]
+#     numerator = if penalty
+#         (
+#             sum(term_zkz_logY_sq)[1] - 2.0 * μ_new * sum(term_zkz_logY)[1] +
+#             (μ_new)^2 * sum(term_zkz)[1] + (pen_pararms_jk[1] - 1)
+#         )
+#     else
+#         (
+#             sum(term_zkz_logY_sq)[1] - 2.0 * μ_new * sum(term_zkz_logY)[1] +
+#             (μ_new)^2 * sum(term_zkz)[1]
+#         )
+#     end
+#     tmp = numerator / demominator
+#     σ_new = sqrt(maximum([0.0, tmp]))
+
+#     return NormalExpert(μ_new, σ_new)
+# end
+
+## EM: M-Step, exact observations
+function EM_M_expert_exact(d::NormalExpert,
+    ye, exposure,
+    z_e_obs;
+    penalty=true, pen_pararms_jk=[1.0 1.0])
+
+    # Further E-Step
+    Y_e_obs = ye
+    Y_sq_e_obs = ye .^ 2
+
+    # Update parameters
+    # pos_idx = (ye .!= 0.0)
+    term_zkz = z_e_obs
+    term_zkz_Y = (z_e_obs .* Y_e_obs)
+    term_zkz_Y_sq = (z_e_obs .* Y_sq_e_obs)
+
+    μ_new = sum(skipmissing(term_zkz_Y))[1] / sum(skipmissing(term_zkz))[1]  # may have missing observations
+
+    demominator = penalty ? (sum(term_zkz)[1] + (pen_pararms_jk[2] - 1)) : sum(skipmissing(term_zkz))[1]
+    numerator = if penalty
+        (
+            sum(term_zkz_logY_sq)[1] - 2.0 * μ_new * sum(term_zkz_logY)[1] +
+            (μ_new)^2 * sum(term_zkz)[1] + (pen_pararms_jk[1] - 1)
+        )
+    else
+        (
+            sum(skipmissing(term_zkz_Y_sq))[1] - 2.0 * μ_new * sum(skipmissing(term_zkz_Y))[1] +
+                (μ_new)^2 * sum(skipmissing(term_zkz))[1]
+        )
+    end
+    tmp = numerator / demominator
+    σ_new = sqrt(maximum([0.0, tmp]))
+
+    return NormalExpert(μ_new, σ_new)
+end
