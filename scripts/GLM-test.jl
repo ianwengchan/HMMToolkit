09f719a445f15d7a7c06fa366d151bde9ac1cef4
Sophia Chan <ianweng.chan@gmail.com>
2023-09-22 10:24:39 -0400

    Update GLM-test.jl

--- scripts/GLM-test.jl
+++ scripts/GLM-test.jl
@@ -2,7 +2,7 @@ using DrWatson
 @quickactivate "FitHMM-jl"
 
 include(srcdir("CTHMM.jl"))
-using CSV, DataFrames, Dates, Statistics, LinearAlgebra, Distributions, CategoricalArrays, GLM, QuasiGLM, Random
+using CSV, DataFrames, Dates, Statistics, LinearAlgebra, Distributions, CategoricalArrays, GLM, QuasiGLM, Random, Base.Threads
 using .CTHMM
 
 # data = DataFrame(y = rand(100).* 3, x = categorical(repeat([1,2,3,4], 25)))
@@ -17,97 +17,87 @@ include(srcdir("CTHMM-learn-nij-taui.jl"))
 include(srcdir("CTHMM-learn-Q.jl"))
 include(srcdir("CTHMM-learn-EM.jl"))
 include(srcdir("CTHMM-simulation.jl"))
+include(srcdir("CTHMM-learn-cov-EM.jl"))
 # include(srcdir("CTHMM-build-cov-Q.jl"))   # imported in CTHMM.jl to use @check_args; call CTHMM.build_cov_Q instead
 
-α = [0.25 0.5;
-    -0.25 0.35]
+α_true = [0.25 0.5 -0.3;
+    0 0 0]
 
 Random.seed!(1234)
 subject_df = DataFrame(subject_ID = collect(1:1:50),
                         intercept = 1,
-                        covariate1 = rand(Distributions.Uniform(0, 1), 50))
+                        covariate1 = rand(Distributions.Uniform(0, 2), 50),
+                        covariate2 = rand(Distributions.Normal(1, 1), 50))
 
 # just assume 1 π_list for all drivers for now
-π_list = [0.7; 0.3]
-state_list = [CTHMM.NormalExpert(0, 1) CTHMM.NormalExpert(-3, 2)]
-
-covariate_list = ["intercept", "covariate1"]
-Random.seed!(1234)
-df_sim = sim_dataset_Qn(α, subject_df, covariate_list, π_list, state_list, 30)
-
-
-α_init = [0.25 0.5;
-        -0.25 0.35]
-
-distinct_time_list = CTHMM_precompute_distinct_time_list(df_sim.time_interval)
-num_state = size(π_list, 1)
-num_dim = size(state_list, 1)
-num_distinct_time = size(distinct_time_list, 1) # assume one Q for all time series
-
-group_df = groupby(df_sim, :subject_ID)
-num_subject = size(group_df, 1)
-distinct_time_list = Array{Any}(undef, num_subject) # the distinct_time_list depends on the subject
-for n = 1:num_subject
-    distinct_time_list[n] = CTHMM_precompute_distinct_time_list(group_df[n].time_interval)
-end
-
-for i = 1:num_state
-    subject_df[:, string("tau", i)] = missings(Float64, nrow(subject_df))
-    for j = 1:num_state
-        subject_df[:, string("N", i, j)] = missings(Float64, nrow(subject_df))
-    end
-end
-
+π_list_true = [0.7; 0.3]
+state_list_true = [CTHMM.NormalExpert(0, 1) CTHMM.NormalExpert(-3, 2)]
 response_list = ["response1"]
 
-# first, do E-step for all drivers, saving Svi to df; need to compute Etij separately for each subject
-ll_em_temp, Etij = CTHMM_batch_decode_Etij_for_cov_subjects(1, df_sim, response_list, subject_df, covariate_list, α, π_list, state_list)
+covariate_list = ["intercept", "covariate1", "covariate2"]
+Random.seed!(1234)
+df_sim = sim_dataset_Qn(α_true, subject_df, covariate_list, π_list_true, state_list_true, 50)
 
-## M-step, with last estimated parameters
-## part 1a: learning initial state probabilities π_list
-for i in 1:num_state
-    π_list[i] = sum(df_sim.start .* df_sim[:, string("Sv", i)])
-end
-π_list = π_list ./ sum(π_list)
 
-ll_em = CTHMM_batch_decode_for_cov_subjects(1, df_sim, response_list, subject_df, covariate_list, α, π_list, state_list)
+α_init = [0.26 0.7 0.2; 0.0 0.0 0.0]
+π_list_init = [0.7; 0.3]
 
-s = ll_em - ll_em_temp > 0 ? "+" : "-"
-pct = abs((ll_em - ll_em_temp) / ll_em_temp) * 100
-# if (print_steps > 0) & (iter % print_steps == 0)
-#     @info(
-#         "Iteration $(iter), updating π_list: $(ll_em_temp) ->  $(ll_em), ( $(s) $(pct) % )"
-#     )
-# end
-ll_em_temp = ll_em
+state_list_init = [CTHMM.NormalExpert(0, 1) CTHMM.NormalExpert(0, 1)]
 
-α_old = copy(α) .- Inf
+fitted = CTHMM_learn_cov_EM(df_sim, response_list, subject_df, covariate_list, α_init, π_list_init, state_list_init; 
+    max_iter = 1000, α_max_iter = 5, penalty = false)
 
-α_iter = 0
+# (α_fit = [0.24804284370256433 0.5153400348290043; 0.0 0.0], π_list_fit = [0.6986277718858532, 0.30137222811414677], state_list_fit = Main.CTHMM.NormalExpert{Float64}[Main.CTHMM.NormalExpert{Float64}(-0.004642361128551442, 1.004177340221124) Main.CTHMM.NormalExpert{Float64}(-2.9822755795314464, 2.0143185017758656)], converge = true, iter = 952, ll = -296175.08295187063)
 
-# while (α_iter < α_max_iter) && (sum((α - α_old) .^ 2) > 1e-10)
+# [0.2556727617381064 0.5172955793133771; 0.0 0.0]
 
-    α_iter = α_iter + 1
-    α_old = copy(α)
+# (α_fit = [0.2390660768692948 0.5453853920616729; 0.0 0.0], π_list_fit = [0.7408893658937321, 0.25911063410626795], state_list_fit = Main.CTHMM.NormalExpert{Float64}[Main.CTHMM.NormalExpert{Float64}(0.004432273383086754, 0.9884524495459106) Main.CTHMM.NormalExpert{Float64}(-2.962825409049089, 2.0180029547281224)], converge = true, iter = 333, ll = -296756.32455303636)
 
-    # have to find Nij_mat and taui_list for each subject
-    subject_df = CTHMM_learn_cov_nij_taui(num_state, num_subject, subject_df, covariate_list, distinct_time_list, α, Etij)
+
+α_true = [0.25; 0]
+
+Random.seed!(1234)
+subject_df = DataFrame(subject_ID = 1,
+                        intercept = 1)
+
+# just assume 1 π_list for all drivers for now
+π_list_true = [0.7; 0.3]
+state_list_true = [CTHMM.NormalExpert(0, 1) CTHMM.NormalExpert(-3, 2)]
+response_list = ["response1"]
+
+covariate_list = ["intercept"]
+Random.seed!(1234)
+df_sim = sim_dataset_Qn(α_true, subject_df, covariate_list, π_list_true, state_list_true, 1000)
+
+
+α_init = [0.26; 0.0]
+π_list_init = [0.7; 0.3]
+
+state_list_init = [CTHMM.NormalExpert(0, 1) CTHMM.NormalExpert(0, 1)]
+
+fitted_cov = CTHMM_learn_cov_EM(df_sim, response_list, subject_df, covariate_list, α_init, π_list_init, state_list_init; 
+    max_iter = 1000, α_max_iter = 5, penalty = false)
+
 
 
 # # need to reverse the signs for:
 # dff = DataFrame(y = subject_df.tau1 ./ subject_df.N12, x = subject_df.covariate1, w = coalesce.(subject_df.N12, 0.0))
 
+y = subject_df.N12 ./ subject_df.tau1
+X = subject_df[!, covariate_list]
+w = coalesce.(subject_df.tau1, 0.0)
+
+coef(glm(Matrix(X), y, Gamma(), LogLink(), wts = w))
+
+dff = DataFrame(y = subject_df.N12 ./ subject_df.tau1, x = subject_df[!, covariate_list], w = coalesce.(subject_df.tau1, 0.0))
+
 # reversed the responses and weights so can take results directly
 dff = DataFrame(y = subject_df.N12 ./ subject_df.tau1, x = subject_df.covariate1, w = coalesce.(subject_df.tau1, 0.0))
 
-coef(glm(@formula(y ~ x), dff, Gamma(), LogLink(), wts = dff.w))
+coef(glm(@formula(y ~ -1 + x), dff, Gamma(), LogLink(), wts = dff.w))
 
 
 # write for-loop to change the indices and take coefficient estimates
 dff = DataFrame(y = subject_df.N21 ./ subject_df.tau2, x = subject_df.covariate1, w = coalesce.(subject_df.tau2, 0.0))
 
 coef(glm(@formula(y ~ x), dff, Gamma(), LogLink(), wts = dff.w))
-
-
-# α = [0.25 0.5;
-#     -0.25 0.35]
