7961b14be41ba54d051f23bff14438a341101f9b
Sophia Chan <ianweng.chan@gmail.com>
2023-09-22 15:18:50 -0400

    update module

--- src/fitting-utils.jl
+++ src/fitting-utils.jl
@@ -6,17 +6,6 @@ include(srcdir("CTHMM.jl"))
 using .CTHMM
 using Logging, Dates, JLD2
 
-include(srcdir("CTHMM-precompute-distinct-time.jl"))
-include(srcdir("CTHMM-precompute.jl"))
-include(srcdir("CTHMM-decode-forward-backward.jl"))
-include(srcdir("CTHMM-decode-viterbi.jl"))
-include(srcdir("CTHMM-batch-decode.jl"))
-include(srcdir("CTHMM-learn-nij-taui.jl"))
-include(srcdir("CTHMM-learn-Q.jl"))
-include(srcdir("CTHMM-learn-EM.jl"))
-include(srcdir("CTHMM-simulation.jl"))
-include(srcdir("CTHMM-learn-cov-EM.jl"))
-
 export
        fit_CTHMM_covariate_model
 
@@ -28,7 +17,7 @@ function fit_CTHMM_covariate_model(df, response_list, subject_df, covariate_list
         @info(now())
         @info("Fitting $(save_name) started.")
         try
-            result = CTHMM_learn_cov_EM(df, response_list, subject_df, covariate_list, α, π_list, state_list; kwargs...)
+            result = CTHMM.CTHMM_learn_cov_EM(df, response_list, subject_df, covariate_list, α, π_list, state_list; kwargs...)
             @save "$(save_name)" * ".JLD2" result
         catch
         end
